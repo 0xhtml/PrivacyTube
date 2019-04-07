f3cab976922c44187eb1a5fa1765715773decf70
0xhtml <34682885+0xhtml@users.noreply.github.com>
2019-04-07 19:13:29 +0200

    Strip apikey on load

--- init.php
+++ init.php
@@ -5,6 +5,6 @@ require_once "classes/API/API.php";
 require_once "classes/API/APIVideo.php";
 
 $key_file = fopen("../key.txt", "r");
-$key = fread($key_file, filesize("../key.txt"));
+$key = str_replace([" ", "\n", "\r"], "", fread($key_file, filesize("../key.txt")));
 fclose($key_file);
 $API = new API($key);
