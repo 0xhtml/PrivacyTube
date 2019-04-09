387d0618622a8f19c387d857dc02093f5d1b08f4
0xhtml <34682885+0xhtml@users.noreply.github.com>
2019-04-09 22:11:07 +0200

    Added cleanup script

---
+++ cleanup.php
@@ -0,0 +1,14 @@
+<?php
+$key_file = fopen("key.txt", "r");
+$key = str_replace(["\n", "\r"], "", fgets($key_file));
+$client_id = str_replace(["\n", "\r"], "", fgets($key_file));
+$client_secret = str_replace(["\n", "\r"], "", fgets($key_file));
+$mysql_pass = str_replace(["\n", "\r"], "", fgets($key_file));
+fclose($key_file);
+$mysqli = mysqli_connect("localhost", "PrivacyTube", $mysql_pass, "PrivacyTube");
+$statement = $mysqli->prepare("DELETE FROM cache WHERE date < (CURRENT_TIMESTAMP - INTERVAL 2 HOUR - INTERVAL 5 MINUTE)");
+if ($statement->execute()) {
+    echo "Success!";
+} else {
+    echo "Error: $statement->error";
+}
