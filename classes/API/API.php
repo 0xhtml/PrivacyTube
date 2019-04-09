91df989ea88fd4bb83de50564ab672b9cba0aaca
0xhtml <34682885+0xhtml@users.noreply.github.com>
2019-04-09 18:14:35 +0200

    Better caching

--- classes/API/API.php
+++ classes/API/API.php
@@ -13,11 +13,9 @@ class API {
 
     public function get(string $url, array $params, bool $save = true)
     {
-        $params["key"] = $this->key;
-        $url = self::URL . $url . "?" . http_build_query($params);
-
-        $statement = $this->mysqli->prepare("SELECT * FROM cache WHERE url = ? AND date > (CURRENT_TIMESTAMP - INTERVAL 1 HOUR) ORDER BY date LIMIT 1");
-        $statement->bind_param("s", $url);
+        $statement = $this->mysqli->prepare("SELECT * FROM cache WHERE url = ? AND params = ? AND date > (CURRENT_TIMESTAMP - INTERVAL 1 HOUR) LIMIT 1");
+        $params_json = json_encode($params);
+        $statement->bind_param("ss", $url, $params_json);
         if ($statement->execute()) {
             $result = $statement->get_result();
             if ($result->num_rows === 1) {
@@ -25,14 +23,17 @@ class API {
             }
         }
 
+        $params["key"] = $this->key;
+        $url = self::URL . $url . "?" . http_build_query($params);
+
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $data = curl_exec($curl);
         curl_close($curl);
 
         if ($save) {
-            $statement = $this->mysqli->prepare("INSERT INTO cache(url, data) VALUES (?, ?)");
-            $statement->bind_param("ss", $url, $data);
+            $statement = $this->mysqli->prepare("INSERT INTO cache(url, params, data) VALUES (?, ?, ?)");
+            $statement->bind_param("sss", $url, $params_json, $data);
             $statement->execute();
         }
 
