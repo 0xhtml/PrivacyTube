0b9ade5def45a5f0cc6b8eff530df16876a37a80
0xhtml <34682885+0xhtml@users.noreply.github.com>
2019-04-10 17:51:13 +0200

    Changed headers

--- web/dl.php
+++ web/dl.php
@@ -10,14 +10,9 @@ if (isset($_GET["v"]) and strlen($_GET["v"]) == 11) {
     }
     $file_size = (string)(filesize("../dl/" . $_GET["v"] . ".mp4"));
     header("Content-Type: video/mp4");
-    header("Accept-Ranges: bytes");
-    header("Content-Length: $file_size");
-    header("Content-Disposition: inline;");
-    header("Content-Range: bytes .$file_size");
-    header("Content-Transfer-Encoding: binary\n");
-    header("Connection: close");
     readfile("../dl/" . $_GET["v"] . ".mp4");
 } elseif (isset($_GET["url"])) {
+    header("Content-Type: image/jpg");
     $curl = curl_init($_GET["url"]);
     curl_exec($curl);
     curl_close($curl);
