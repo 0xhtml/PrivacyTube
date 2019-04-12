5048453bb5b4cf9bceadfc189ba5154cfa2c5a25
0xhtml <34682885+0xhtml@users.noreply.github.com>
2019-04-12 21:10:08 +0200

    Changed dl.php to fix a bug while playing videos

--- web/dl.php
+++ web/dl.php
@@ -1,16 +1,15 @@
 <?php
-if (!is_dir("../dl")) {
-    mkdir("../dl");
+if (!is_dir("dl")) {
+    mkdir("dl");
 }
 
 if (isset($_GET["v"]) and strlen($_GET["v"]) == 11) {
     set_time_limit(0);
-    if (!file_exists("../dl/" . $_GET["v"] . ".mp4")) {
-        exec("youtube-dl --format mp4 -o \"../dl/" . $_GET["v"] . ".mp4\" \"" . $_GET["v"] . "\"");
+    if (!file_exists("dl/" . $_GET["v"] . ".mp4")) {
+        exec("youtube-dl --format mp4 -o \"dl/" . $_GET["v"] . ".mp4\" \"" . $_GET["v"] . "\"");
     }
     $file_size = (string)(filesize("../dl/" . $_GET["v"] . ".mp4"));
-    header("Content-Type: video/mp4");
-    readfile("../dl/" . $_GET["v"] . ".mp4");
+    header("Location: dl/" . $_GET["v"] . ".mp4");
 } elseif (isset($_GET["url"])) {
     $curl = curl_init($_GET["url"]);
     curl_setopt($curl, CURLOPT_HEADERFUNCTION, function ($curl, $header) {
