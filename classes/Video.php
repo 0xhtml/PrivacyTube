3f076f8274bc705bb630e6bf0d4a18afe9a97537
0xhtml <34682885+0xhtml@users.noreply.github.com>
2019-08-27 17:28:19 +0200

    Simplify Video::fromUser()

--- classes/Video.php
+++ classes/Video.php
@@ -99,57 +99,12 @@ class Video
         $result = array();
 
         $subscriptions = $user->getSubscriptions($system);
-        $subscriptions = join(",", $subscriptions);
 
-        $data = $system->api("/channels", array("id" => $subscriptions, "part" => "contentDetails"));
-        if (!isset($data->items)) {
-            die("Can't load subscribed channels of user");
-        }
+        foreach ($subscriptions as $subscription) {
+            $videos = self::fromChannel(Channel::fromId($subscription, $system), $system);
 
-        foreach ($data->items as $channel) {
-            if (!isset(
-                $channel->id,
-                $channel->contentDetails,
-                $channel->contentDetails->relatedPlaylists,
-                $channel->contentDetails->relatedPlaylists->uploads
-            )) {
-                die("Can't load subscribed channels uploads");
-            }
-
-            $videos = $system->api("/playlistItems", array("playlistId" => $channel->contentDetails->relatedPlaylists->uploads, "part" => "snippet", "maxResults" => 50));
-            if (!isset($videos->items)) {
-                die("Can't load videos of subscribed channel $channel->id");
-            }
-
-            foreach ($videos->items as $video) {
-                if (!isset(
-                    $video->snippet,
-                    $video->snippet->publishedAt,
-                    $video->snippet->title,
-                    $video->snippet->description,
-                    $video->snippet->channelId,
-                    $video->snippet->channelTitle,
-                    $video->snippet->thumbnails,
-                    $video->snippet->thumbnails->medium,
-                    $video->snippet->thumbnails->medium->url,
-                    $video->snippet->resourceId,
-                    $video->snippet->resourceId->videoId
-                )) {
-                    die("Can't load video of subscribed channel $channel->id");
-                }
-
-                $result[strtotime($video->snippet->publishedAt)] = new Video(
-                    $video->snippet->resourceId->videoId,
-                    null,
-                    $video->snippet->title,
-                    $video->snippet->description,
-                    new Channel($video->snippet->channelId, $video->snippet->channelTitle, null, null, null),
-                    strtotime($video->snippet->publishedAt),
-                    null,
-                    null,
-                    null,
-                    $video->snippet->thumbnails->medium->url
-                );
+            foreach ($videos as $video) {
+                $result[$video->getDate()] = $video;
             }
         }
 
