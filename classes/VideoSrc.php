a82664f0a5bf6815aaa411041b43d473f5679684
0xhtml <34682885+0xhtml@users.noreply.github.com>
2019-09-29 20:49:55 +0200

    Load more videos

--- classes/VideoSrc.php
+++ classes/VideoSrc.php
@@ -6,19 +6,41 @@ class VideoSrc
 
     public static function fromId(string $id): array
     {
-        $response = file_get_contents("https://www.youtube.com/get_video_info?video_id=" . urlencode($id));
-        parse_str($response, $data);
-        if (!isset($data["player_response"])) {
-            die("Can't load VideoSrc from id ($id)");
-        }
-        $playerdata = json_decode($data["player_response"]);
-        if (!isset($playerdata->streamingData) or !isset($playerdata->streamingData->formats) or !is_array($playerdata->streamingData->formats)) {
-            die("Can't load VideoSrc from id ($id)");
-        }
+        $curl = curl_init("https://www.youtube.com/watch?v=" . urlencode($id) . "&pbj=1");
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            "X-YouTube-Client-Name: 1",
+            "X-YouTube-Client-Version: 2.20190926.06.01"
+        ));
+        $data = json_decode(curl_exec($curl));
+        curl_close($curl); 
+
         $result = array();
-        foreach ($playerdata->streamingData->formats as $src) {
-            if (isset($src->url)) {
-                $result[] = new self($src->url);
+        foreach ($data as $value) {
+            if (!isset(
+                $value->player,
+                $value->player->args,
+                $value->player->args->player_response
+            )) {
+                continue;
+            }
+            $player_data = json_decode($value->player->args->player_response);
+            if (!isset(
+                $player_data->streamingData,
+                $player_data->streamingData->formats,
+                $player_data->streamingData->adaptiveFormats
+            )) {
+                continue;
+            }
+            foreach ($player_data->streamingData->formats as $src) {
+                if (isset($src->url)) {
+                    $result[] = new self($src->url);
+                }
+            }
+            foreach ($player_data->streamingData->adaptiveFormats as $src) {
+                if (isset($src->url)) {
+                    $result[] = new self($src->url);
+                }
             }
         }
         return $result;
