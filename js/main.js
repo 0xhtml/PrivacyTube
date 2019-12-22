5d22b9fa0bcee2adbd181e66b58d67bf032941ca
0xhtml <34682885+0xhtml@users.noreply.github.com>
2019-12-22 20:42:01 +0100

    Basic structure and functions

---
+++ js/main.js
@@ -0,0 +1,84 @@
+const PROXIE = "https://cors-anywhere.herokuapp.com/"
+
+window.addEventListener("load", () => {
+    setupStorage();
+    document.querySelectorAll('[data-subscriptions]').forEach(renderSubscriptions);
+});
+
+function setupStorage() {
+    setupStorageItem("subscriptions", {});
+}
+
+function setupStorageItem(key, value) {
+    if (localStorage.getItem(key) == null) {
+        setItem(key, value);
+    }
+}
+
+function setItem(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+
+function getItem(key) {
+    return JSON.parse(localStorage.getItem(key));
+}
+
+function renderSubscriptions(elem) {
+    const subscriptions = JSON.parse(localStorage.getItem("subscriptions"));
+    var videos = [];
+    for (const subscription in subscriptions) {
+        if (subscriptions.hasOwnProperty(subscription)) {
+            videos.push(...subscriptions[subscription].videos);
+        }
+    }
+    for (const video of videos) {
+        const div = document.createElement("p");
+        div.innerHTML = video.title;
+        elem.appendChild(div);
+        if (elem.childElementCount == elem.getAttribute("data-subscriptions")) {
+            break;
+        }
+    }
+}
+
+function parseChannel(json) {
+    var metadata = json[1].response.metadata.channelMetadataRenderer;
+    var tab = json[1].response.contents.twoColumnBrowseResultsRenderer.tabs[1];
+    var contents = tab.tabRenderer.content.sectionListRenderer.contents[0];
+    var items = contents.itemSectionRenderer.contents[0].gridRenderer.items;
+    var videos = [];
+    for (const item of items) {
+        videos.push({
+            id: item.gridVideoRenderer.videoId,
+            title: item.gridVideoRenderer.title.simpleText,
+            thumbnail: item.gridVideoRenderer.thumbnail.thumbnails[0].url
+        });
+    }
+    return {
+        id: metadata.externalId,
+        title: metadata.title,
+        thumbnail: metadata.avatar.thumbnails[0].url,
+        videos: videos
+    };
+}
+
+function subscribe(channel) {
+    fetch(
+        PROXIE + "https://youtube.com/channel/" + channel + "/videos?pbj=1",
+        {
+            headers: {
+                "X-YouTube-Client-Name": "1",
+                "X-YouTube-Client-Version": "2.20190926.06.01"
+            }
+        }
+    ).then(
+        res => {return res.json()}
+    ).then(
+        data => {
+            var subscriptions = getItem("subscriptions");
+            const channel = parseChannel(data);
+            subscriptions[channel.id] = channel;
+            setItem("subscriptions", subscriptions);
+        }
+    )
+}
