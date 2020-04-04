8bb13026b57e8ad5535c94ea3bb7722097130d14
0xhtml <34682885+0xhtml@users.noreply.github.com>
2020-04-05 00:15:43 +0200

    First page

---
+++ js/index.js
@@ -0,0 +1,21 @@
+var app = new Vue({
+    el: 'main',
+    data: {
+        subscriptions: []
+    }
+});
+
+var dev_subscriptions = ["UCDrekHmOnkptxq3gUU0IyfA", "UCY1kMZp36IQSyNx_9h4mpCg"];
+
+dev_subscriptions.forEach(channel => {
+    var req = new XMLHttpRequest();
+    req.open("GET", "https://invidio.us/api/v1/channels/" + channel);
+    req.responseType = "json";
+    req.addEventListener("load", () => {
+        Array.prototype.push.apply(app.subscriptions, req.response.latestVideos);
+        app.subscriptions.sort((a, b) => {return b.published-a.published});
+        app.subscriptions.splice(20);
+        console.log(app.subscriptions[0]);
+    });
+    req.send();
+});
