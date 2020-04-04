875bad0f4de8f6aa434d2dc9787a67a398a5fe3d
0xhtml <34682885+0xhtml@users.noreply.github.com>
2020-04-05 00:15:34 +0200

    Delete files

--- web/api/subscriptions.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "../../classes/APIUser.php";
-require_once "../../classes/Main.php";
-
-$main = new Main();
-$user = new APIUser($main);
-header("Content-Type: application/json");
-echo json_encode($user->getSubscriptions($main));
