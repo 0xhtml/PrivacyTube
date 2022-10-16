e4568ca52a18de2ed69f5bda87c5fabc81352e75
0xhtml <34682885+0xhtml@users.noreply.github.com>
2022-10-17 01:40:47 +0200

    Show message if no search results where found

--- js/components/search.js
+++ js/components/search.js
@@ -5,13 +5,14 @@ export default {
         <main v-if="results === null"><p>Loading search results...</p></main>
         <main v-else>
             <h2>Search results for {{$route.query.q}}</h2>
-            <div class="row">
+            <div v-if="results.length > 0" class="row">
                 <div v-for="channel in results">
                     <vue-img :sources="channel.authorThumbnails" :width="10"></vue-img>
                     <h3>{{channel.author}}</h3>
                     <subscribe :channel="channel.authorId"></subscribe>
                 </div>
             </div>
+            <p v-else>No search results</p>
         </main>
     `,
     components: {
